ac04be6970026d3fb89b641754aa0360bf547622
Felipe <felipe.g.te@gmail.com>
2017-12-18 17:49:49 -0200

    Head's up!

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# algodinha
-:chart_with_upwards_trend: Algoritmo de negociação Bitcoin para Foxbit / Blinktrade
+# Algodinha
+
+Algoritmo de negociação automatizada para Bitcoin - via Foxbit / Blinktrade.
+
+## Sobre
+
+O robô executa a estratégia de [Scalper](https://www.daytraderpro.com.br/blog/o-que-e-scalping/), visando pequenos lucros no maior número de ordens possíveis.
+
+Execute literalmente por sua conta e risco. 
+
+## Instalando
+
+Baixe o fonte ou clone o Repo e baixe as dependêcias usando:
+
+```javascript
+npm install
+```
+
+## Configurando
+
+### Segurança
+Antes de executar o robô você precisa gerar suas credenciais de acesso [aqui](https://foxbit.exchange/#api). Estes dados devem ser salvos em um arquivo chamado `api.json` na raiz da biblioteca utilizando o seguinte formato:
+
+```javascript
+{ 
+    "user": "",
+    "password": "",
+    "secret": ""
+}
+```
+
+### Parametrização
+Você deve também parametrizar a execução considerando os limites e riscos que você está disposto a correr com a ferramenta. Os principais parâmetros que coordenam a proporção risco/lucro são os seguintes:
+
+```
+valorMaximoCompra : 70000,
+maximoGastos : 2500,
+valorOrdem : 100,
+lucroEsperado : 0.06,
+```
+
+De fato, como regra geral, estes são os únicos parâmetros que você precisaria alterar para realizar a execução.
+
+## Executando
+
+Para executar, basta utilizar o seguinte comando:
+
+```
+node algodinha.js
+```
+
---
+++ algodinha.js
@@ -0,0 +1,459 @@
+
+/// TODO: 
+///     - Detectar queda de conexão (internet fora do ar);
+///     - Enviar email para relatar trades e erros;
+///     - Rastrear ponto de inversão para entrar comprado;
+///     - Obter o valor real gasto;
+
+var AlgoDinha = function() { 
+    
+    /// WebSocket
+    var BlinkTradeWS = require("blinktrade").BlinkTradeWS,
+        blinktradeWs = new BlinkTradeWS( { prod: true }),
+        colors = require('colors');
+    
+    colors.setTheme({
+        erro: ["red", "underline"],
+        aviso: ["yellow", "bold"],
+        ok: ["green", "bold"],
+        comprado: ["blue"],
+        vendido: ["magenta"],
+        titulo: ["white", "bold"]
+    });
+
+    var params = {
+
+        security : require("./api.json"), 
+        compras : [],
+        ultimaMelhorOferta : null,
+        book : { bids:[], asks:[] },
+        taxaDaCorretora : 0.05,
+        aguardandoOrdem : false,
+        comprado : false,
+        subindo : false,
+
+        valorMaximoCompra : 70000,
+        maximoGastos : 2500,
+        valorOrdem : 100,
+        lucroEsperado : 0.06
+        
+    };
+       
+    function pln(str, warn) { 
+        if (!warn) { 
+            console.info(str);
+        } else { 
+            console.warn(str.aviso);
+        }
+    }
+
+    function trataNegociacao(ordem) { 
+        blinktradeWs.balance().then(function(extrato) { 
+            if (ordem.Side == "1") { 
+                
+                var saldoAnterior = obterVolumeTotal();
+                var disponivel = ((extrato.Available.BTC ? extrato.Available.BTC : extrato["4"].BTC) / 1e8);
+                
+                if (saldoAnterior > 0) { 
+                    
+                    var executado = ordem.LastShares / 1e8;
+                    disponivel = (disponivel + executado) - (saldoAnterior + executado);
+
+                }
+
+                adicionarCompra((ordem.LastPx / 1e8), disponivel);
+            } else { 
+                console.log("Vendeu", ordem);
+            }
+            params.aguardandoOrdem = false;
+        });
+    }
+    
+    function onPartial(ordem) {
+        trataNegociacao(ordem);
+    }
+    
+    function onExecution(ordem) {
+        trataNegociacao(ordem);
+    }
+    
+    function atualizaBook(item){ 
+        
+        /// Só queremos o topo do book
+        if (item && ((item.index == 1) || (item.index == 0))) { 
+
+            if (item.side == "buy") { 
+                params.book.bids[0] = item.price;
+            } else if (item.side == "sell") { 
+                params.book.asks[0] = item.price;
+            }
+
+            trataOrdens();
+
+        }
+    }
+    
+    function obterValorVenda() { 
+        var valorMedio = obterValorMedioCompras();
+        var baseCalculo = (obterVolumeTotal() * valorMedio) * ((params.taxaDaCorretora * 2) + params.lucroEsperado);
+        return valorMedio + baseCalculo;
+    }
+    
+    function obterValorTotalGasto() { 
+        
+        if (params.compras && params.compras.length > 0) { 
+            return params.compras.length * params.valorOrdem;
+        }
+    
+        return 0;
+    
+    }
+    
+    function obterValorMedioCompras() { 
+    
+        if (params.compras && params.compras.length > 0) { 
+            var valorTotal = 0;
+            for (var i = 0; i < params.compras.length; i++) { 
+                valorTotal += params.compras[i].valor * params.compras[i].volume;
+            }
+            return valorTotal / obterVolumeTotal();
+        }
+    
+        return 0;
+    }
+    
+    function obterVolumeTotal() { 
+        
+        if (params.compras && params.compras.length > 0) { 
+            var volumeTotal = 0;
+            for (var i = 0; i < params.compras.length; i++) { 
+                volumeTotal += params.compras[i].volume;
+            }
+            return volumeTotal;
+        }
+        
+        return 0;
+    }
+    
+    function obterValorMenorCompra() { 
+        if (params.compras && params.compras.length > 0) { 
+            var menorCompra = 0;
+                for (var i = 0; i < params.compras.length; i++) { 
+                    if (menorCompra == 0 || (params.compras[i].valor < menorCompra)) { 
+                        menorCompra = params.compras[i].valor;
+                    }
+                }
+            return menorCompra;
+        }
+        return 0;    
+    }
+    
+    function adicionarCompra(valor, volume) { 
+        params.comprado = true;
+        params.compras.push({valor:valor, volume:volume});
+    }
+    
+    function adicionarOrdemVenda(preco, volume, okDel, nokDel) { 
+        adicionarOrdem(preco, volume, "2", okDel, nokDel);
+    }
+    
+    function adicionarOrdemCompra(preco, volume, okDel, nokDel) { 
+        adicionarOrdem(preco, volume, "1", okDel, nokDel);
+    }
+    
+    function limparCompras() { 
+        params.compras.length = 0;
+    }
+    
+    function trataOrdens() { 
+        
+        pln("----------------------------------------------------------------------------------------------------------------".titulo);
+        pln((new Date().toLocaleString() + " - Valor máximo para compra: R$ " + params.valorMaximoCompra + ", Máximo de gastos: R$ " + params.maximoGastos + " - Valor investido: R$ " + obterValorTotalGasto() + ".").titulo);
+        pln("");
+        
+        if (params.aguardandoOrdem) { 
+            pln("Aguardando execução da última ordem");
+            pln("");
+            return;
+        }
+    
+        var o = params.book;
+        var valorVenda = obterValorVenda();
+        var valorMedioDaCarteira = obterValorMedioCompras();
+        var melhorOfertaCompraAtual = o.bids[0];
+        var melhorOfertaVendaAtual = o.asks[0];
+    
+        pln("STATUS ATUAL DA CARTEIRA: ");
+        pln("    - Valor médio: R$ " + valorMedioDaCarteira.toFixed(3));
+        pln("    - Volume total: " + obterVolumeTotal());
+        pln("    - Target: R$ " + valorVenda.toFixed(2));
+        pln("");
+        pln("STATUS ATUAL DO MERCADO");
+        pln("     - Compra: " + melhorOfertaCompraAtual.toFixed(3));
+        pln("     - Venda: " + melhorOfertaVendaAtual.toFixed(3));
+        pln("     - Aguardando uma melhora de " + (((valorVenda - melhorOfertaCompraAtual)/valorVenda)*100).toFixed(2) + "%");
+        pln("");
+        
+        /// Caso já tenhamos uma ordem executada
+        if (params.comprado) { 
+            
+            pln("Executando comprado...".comprado);
+            if (melhorOfertaCompraAtual > valorVenda) { 
+                if (params.subindo) { 
+                    if (params.ultimaMelhorOferta <= melhorOfertaCompraAtual) { 
+    
+                        pln("Mercado continua subindo!!");
+                        params.ultimaMelhorOferta = melhorOfertaCompraAtual;
+    
+                    } else { 
+    
+                        /// Ops, começando a descer, melhor sair!!
+                        pln("Vendendo por " + melhorOfertaCompraAtual, true);
+    
+                        /// Atualiza o valor máximo que podemos usar pra comprar pois o mercado se provou neste novo patamar
+                        if (melhorOfertaCompraAtual > params.valorMaximoCompra) { 
+                            params.valorMaximoCompra = melhorOfertaCompraAtual;
+                        }
+    
+                        /// Vende tudo!!!
+                        adicionarOrdemVenda(melhorOfertaCompraAtual, obterVolumeTotal(), function(r) { 
+                            if (r) { 
+                                limparCompras();
+                                params.comprado = false;
+                                params.subindo = false;
+                                console.log("FINALIZADO!!!!!!!!!");
+                            }
+                        }, function() { });
+    
+                    }
+    
+                } else { 
+    
+                    /// Inicia leitura do mercado
+                    pln("Mercado subindo!!");
+                    params.ultimaMelhorOferta = melhorOfertaCompraAtual;
+                    params.subindo = true;
+                }
+    
+            } else { 
+    
+                if (obterValorTotalGasto() >= params.maximoGastos) { 
+    
+                    /// Gastamos tudo...
+                    pln(" - Gastamos todo o orçamento. Agora tem que rezar.");
+    
+                } else { 
+    
+                    
+                    /// Vamos tentar diminuir o custo médio comprando mais abaixo do preço de entrada
+                    var thresholdNovaCompra = (obterValorMenorCompra() - 50);
+                    
+                    if (melhorOfertaVendaAtual > valorMedioDaCarteira) { 
+                        pln("O mercado está subindo, e ele tem talento pra isso!! Melhor oferta de venda atual: R$ " + melhorOfertaVendaAtual + ".");
+                    }
+                    else if (melhorOfertaVendaAtual < thresholdNovaCompra) { 
+    
+                        pln("- Tentando melhorar média de saída...");
+                        pln("  Adicionando posição por " + melhorOfertaVendaAtual, true);
+                        var volume = (params.valorOrdem / melhorOfertaVendaAtual);
+                        adicionarOrdemCompra(melhorOfertaVendaAtual, volume, function(r) { 
+                            if (r) { 
+    
+                                params.subindo = false;
+    
+                            } else { 
+                                console.warn("  Deu RUIM", r);
+                            }
+                        }, function() { });
+    
+                    } else { 
+    
+                        pln("  Muito caro. Melhor oferta atual: R$ " + melhorOfertaVendaAtual + ", menor valor comprado: R$ " + obterValorMenorCompra() + ". Comprando novamente em: R$ " + thresholdNovaCompra);
+    
+                    }
+                }
+            }
+        }
+    
+        /// Estamos esperando o momento de entrar no mercado e montar posição
+        else { 
+    
+            pln("Executando sem posição...".vendido);
+   
+            if (melhorOfertaVendaAtual <= params.valorMaximoCompra) { 
+    
+                pln("Comprando por " + melhorOfertaVendaAtual, true);
+                var volume = (params.valorOrdem / melhorOfertaVendaAtual);
+                adicionarOrdemCompra(melhorOfertaVendaAtual, volume, function(r) { 
+                    if (r) { 
+    
+                        params.subindo = false;
+                        
+                    } else { 
+                        console.warn("Deu RUIM", r);
+                    }
+                }, function() { });
+    
+            } else { 
+    
+                pln("Muito caro. Melhor oferta atual: " + melhorOfertaVendaAtual + ".");
+    
+            }
+        }
+    }
+    
+    function adicionarOrdem(preco, volume, tipo, okDel, nokDel) { 
+        try{
+            
+            if (preco == 0) { 
+                nokDel({});
+                return;
+            }
+
+            params.aguardandoOrdem = true;
+            blinktradeWs.sendOrder({
+                side: tipo,
+                price: parseInt(preco * 1e8, 10),
+                amount: parseInt(volume * 1e8, 10),
+                symbol: "BTCBRL"
+            }).then(
+                function(ok){ 
+                    console.warn("Ordem colocada com sucesso!".aviso, ok[0]);
+                    okDel(ok);
+                    pln(""); pln(""); pln("");
+                    pln(""); pln(""); pln("");
+                }
+            ).catch(
+                function (nok) { 
+                    console.error("Falha ao enviar ordem".erro, nok); 
+                    nokDel(nok); 
+                }
+            );
+
+        } catch(E) { 
+            console.error("Falha na infra de ordem".erro, nok); 
+            nokDel(E);
+        }
+    }
+
+    function enviarBatida(ultimaExecucao) { 
+        setTimeout(function() { 
+            blinktradeWs.heartbeat(function() { 
+                console.log(colors.dim(" <3  <3  <3  <3  <3  <3  <3  <3  <3  <3 "));
+                enviarBatida();
+            })
+            .catch(function(E) {
+                console.error("Heartbeat falhou".erro, E);
+                publico.iniciar();
+            });
+        }, 10000 /*Every ten seconds*/);
+    }
+
+    function atualizarCarteira(dataBase, okDel, nokDel) { 
+        return blinktradeWs.requestLedger( { pageSize: 200 })
+            .then(function(historico) { 
+                
+                var carteiraTemporaria = {};
+                var livro = historico.LedgerListGrp;
+
+                for (var i = 0; i < livro.length; i++) { 
+                    var item = livro[i];
+
+                    if (new Date(item.Created) > dataBase) { 
+
+                        /// T - Trade
+                        /// TF - Trade Fee
+                        if (!carteiraTemporaria[item.Reference]) { 
+                            carteiraTemporaria[item.Reference] = { valor: 0, volume: 0};
+                        }
+
+                        if (item.Description == "T") { 
+                                if (item.Currency == "BRL") { 
+                                    if (!carteiraTemporaria[item.Reference].volumeOriginal) { 
+                                        console.error("Deu PAU!!!!!! Falta volume original!!".erro);
+                                        nokDel();
+                                        return;
+                                    }
+                                    carteiraTemporaria[item.Reference].valor = (item.Amount / 1e8) / carteiraTemporaria[item.Reference].volumeOriginal;
+                                } else if (item.Currency == "BTC") { 
+                                    carteiraTemporaria[item.Reference].volumeOriginal = item.Amount / 1e8;
+                                    carteiraTemporaria[item.Reference].volume += item.Amount / 1e8;
+                                }
+                            } else if (item.Description == "TF") { 
+                                carteiraTemporaria[item.Reference].volume -= item.Amount / 1e8;
+                            }
+                        }
+                    }
+
+                    for(i in carteiraTemporaria) { 
+                        if (carteiraTemporaria[i].volume > 0) { 
+                            adicionarCompra(carteiraTemporaria[i].valor, carteiraTemporaria[i].volume);
+                        }
+                    }
+                    
+                    okDel();
+                })
+            .catch(function(Exc) {
+                console.error("Deu ruim na posição".erro, Exc);
+                publico.iniciar();
+            });
+    }
+  
+    var publico = { 
+        iniciar : function(ultimaExecucao) { 
+            pln("Iniciando...".titulo);
+                       
+            /// Conecta na Exchange
+            blinktradeWs.connect().then(function() {
+                
+                return blinktradeWs.login({ username: params.security.user, password: params.security.password });
+            
+            }).then(function(logged) {
+                
+                atualizarCarteira(ultimaExecucao, function() { 
+
+                    enviarBatida();
+                    
+                    blinktradeWs.executionReport()
+                        .on("EXECUTION_REPORT:PARTIAL", onPartial)
+                        .on("EXECUTION_REPORT:EXECUTION", onExecution);
+                    
+                    blinktradeWs.subscribeOrderbook(["BTCBRL"])
+                        .on("OB:NEW_ORDER", atualizaBook)
+                        .on("OB:UPDATE_ORDER", atualizaBook)
+                        // .on("OB:TRADE_NEW", atualizaBook)
+                        // .on("OB:DELETE_ORDER", apagaBook)
+                        // .on("OB:DELETE_ORDERS_THRU", apagaBook)
+                        .then(function(fullBook) { 
+                
+                            var dadosDoBook = fullBook.MDFullGrp.BTCBRL;
+                            params.book = { asks:dadosDoBook.asks[0], bids:dadosDoBook.bids[0] };
+                            trataOrdens();
+                
+                        }).catch(function(EE) {
+                            console.error("Erro na assinatura do book.".erro, EE);
+                        });
+
+                }, function() {
+                    console.error("Problema ao obter carteira".erro, e);
+                });
+            })
+            .catch(function(e){ 
+                console.error("Problema ao registrar pra execution report".erro, e);
+            });
+        }
+    };
+
+    return publico;
+}
+
+
+new AlgoDinha()
+    
+    /// Adicionar 2 horas de FUSO
+    .iniciar(new Date("2017-12-18 12:22:03"));
+
+
+
+
+
